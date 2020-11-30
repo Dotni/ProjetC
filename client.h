5247a66156a8529d58b4ac0f41cee0a963f276aa
Dotni <57830936+Dotni@users.noreply.github.com>
2020-11-30 10:51:12 +0100

    division

---
+++ client.h
@@ -0,0 +1,6 @@
+//définition du client
+typedef struct client {
+	char nom[30];						//nom du client
+	struct sejour sej;					//pour la liste chainée des séjours du client
+	struct client *nxtClient;			//pour la liste chainée des clients
+}client;
---
+++ employe.h
@@ -0,0 +1,4 @@
+//défintion des employés 					A REVOIR PLUS TARD
+typedef struct employe{
+	char nom[30];						//nom de l'employé
+}employe;
