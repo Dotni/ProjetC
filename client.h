188110122c22081dab4d43c26c039b41a8ebd4ae
scryper <florian.mazzeo@gmail.com>
2020-12-02 17:13:14 +0100

    data clients

--- client.h
+++ client.h
@@ -3,6 +3,7 @@
 //définition du client
 typedef struct client {
 	char nom[30];						//nom du client
+	char prenom[30];
 	struct sejour sej;					//pour la liste chainée des séjours du client
 	struct client *nxtClient;			//pour la liste chainée des clients
 }client;
