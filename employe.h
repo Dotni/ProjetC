545d0887e27f5c42822d7a0c20afed593e9ebf5e
scryper <florian.mazzeo@gmail.com>
2020-12-02 19:03:01 +0100

    a

---
+++ employe.h
@@ -0,0 +1,11 @@
+#ifndef EMPLOYE_H
+#define EMPLOYE_H
+//e
+//défintion des employés 					A REVOIR PLUS TARD
+typedef struct employe{
+	char nom[30];						//nom de l'employé
+	float salaireParHeure;
+	int nbHeures;
+	int specialisation; // manager, entretien, 
+}employe;
+#endif
