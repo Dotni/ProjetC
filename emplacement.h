d0266f3359080e39b988d2216142d5e21a32478d
scryper <florian.mazzeo@gmail.com>
2020-12-02 18:15:13 +0100

    data

--- emplacement.h
+++ emplacement.h
@@ -1,7 +1,7 @@
 //définition d'un emplacement
 typedef struct emplacement{
-	short type;							//1 pour tente , 2 pour caravane, 3 pour bungalow
-	short electricite;							//1 s'il y a la possibilité d'avoir l'électricité, 0 si non
-	float taille;						//taille de l'emplacement 
-	float prix;							//prix de l'emplacement
+	short type;							//1 pour tente , 2 pour caravane, 3 pour bungalow 				1d
+	short electricite;					//1 s'il y a la possibilité d'avoir l'électricité, 0 si non		1d
+	float taille;						//taille de l'emplacement 										2d
+	float prix;							//prix de l'emplacement											3d
 }emplacement;
