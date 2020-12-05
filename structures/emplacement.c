66cc2a77f2155c4ef523de415c0dcd9d562d3fb7
Martin <martin.maes@student.umons.ac.be>
2020-12-05 16:59:19 +0100

    CA MARCHE ENFIN TABARnAK

--- structures/emplacement.c
+++ structures/emplacement.c
@@ -1,10 +1,24 @@
-#include "emplacement.h"
+#include <stdio.h>
 
 const int tailleEmplacement=30;
-static emplacement empl[30];
 
-void lectureEmplacements(){
-	viderEmplacements();
+typedef struct emplacement{
+	short id;							//id de l'emplacement max 32 767	
+	short type;							//1 pour tente , 2 pour caravane, 3 pour bungalow 				1d
+	short electricite;					//1 s'il y a la possibilité d'avoir l'électricité, 0 si non		1d
+	float taille;						//taille de l'emplacement 										2d
+	float prix;							//prix de l'emplacement											3d
+}emplacement;
+
+void viderEmplacements(emplacement empl[]){
+	int i;
+	for (i=1;i<tailleEmplacement;i++){
+		empl[i].id=0;
+	}
+}
+
+void lectureEmplacements(emplacement empl[]){
+	viderEmplacements(empl);
 	FILE *fEmplacement;
 	fEmplacement = fopen("data/emplacements.dat","r");
 	int i=1;
@@ -15,8 +29,8 @@ void lectureEmplacements(){
 	fclose(fEmplacement);
 }
 
-void affichageListeEmplacement(){
-	lectureEmplacements();
+void affichageListeEmplacement(emplacement empl[]){
+	lectureEmplacements(empl);
 	int i=1;
 	while(empl[i].id!=0){
 		printf("Emplacement %2d : id:%2d  type:%2d  electricite:%2d  taille:%04.2f prix:%05.2f%€\n",i,empl[i].id,empl[i].type,empl[i].electricite,empl[i].taille,empl[i].prix);
@@ -24,9 +38,4 @@ void affichageListeEmplacement(){
 	}
 }
 
-void viderEmplacements(){
-	int i;
-	for (i=1;i<tailleEmplacement;i++){
-		empl[i].id=0;
-	}
-}
+
--- structures/emplacement.h
+++ structures/emplacement.h
@@ -1,14 +1,7 @@
 #ifndef EMPLACEMENT_H
 #define EMPLACEMENT_H
-#include <stdio.h>
 
-typedef struct emplacement{
-	short id;							//id de l'emplacement max 32 767	
-	short type;							//1 pour tente , 2 pour caravane, 3 pour bungalow 				1d
-	short electricite;					//1 s'il y a la possibilité d'avoir l'électricité, 0 si non		1d
-	float taille;						//taille de l'emplacement 										2d
-	float prix;							//prix de l'emplacement											3d
-}emplacement;
+typedef struct emplacement;
 
 void affichageListeEmplacement();
 void lectureEmplacements();
