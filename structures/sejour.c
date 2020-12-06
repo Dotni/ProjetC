6011b530cd9634c9d3b933817774860cd243a3e7
scryper <florian.mazzeo@gmail.com>
2020-12-06 18:15:46 +0100

    zebi

--- structures/sejour.c
+++ structures/sejour.c
@@ -11,14 +11,13 @@ void viderSejours(sejour sejours[], int tailleMax){
 }
 
 int lectureSejours(int tailleMax, sejour sejours[]) {
-	printf("test");
-	int nbSejours = 1, jour, mois, annee;
+	printf("test\n");
+	int nbSejours = 1, jour, mois, annee, tmp, tmp2;
 	char tmpJour[3], tmpMois[3], tmpAnnee[5], tmpDate[11];
 	FILE *fSejour;
 	fSejour = fopen("data/sejour.dat", "r");
 	while(!feof(fSejour)) {
-		fscanf(fSejour, "%3d %1d %1d %2d %2d %4d", &sejours[nbSejours].id, &sejours[nbSejours].formule, &jour, &mois, &annee, &sejours[nbSejours].prix); // a terminer
-		// organisation de sejour.dat ? à quoi correspond le 3eme nombre ?
+		fscanf(fSejour, "%d %d %d %2d %2d %4d %f %d %d", &sejours[nbSejours].id, &sejours[nbSejours].formule, &sejours[nbSejours].nbPersonnes, &jour, &mois, &annee, &sejours[nbSejours].prix, &tmp, &tmp2); 
 		// conversion entier -> char pour créer la date
 		itoa(jour, tmpJour, 10);
 		itoa(mois, tmpMois, 10);
@@ -30,24 +29,27 @@ int lectureSejours(int tailleMax, sejour sejours[]) {
 		strcat(tmpDate, "/");
 		strcat(tmpDate, tmpAnnee);
 		strcpy(sejours[nbSejours].date, tmpDate);
-		printf("%s", sejours[nbSejours].date);
+		printf("%d %d %d %s %6.2f\n", sejours[nbSejours].id, sejours[nbSejours].formule, sejours[nbSejours].nbPersonnes, sejours[nbSejours].date, sejours[nbSejours].prix);
 		nbSejours++;
-		printf("test 2");
+		printf("test 2\n");
 		// problème de boucle 
 	}
 	fclose(fSejour);
-	printf("test  3"); // test non atteind
-	return nbSejours - 1;
+	printf("test  3\n"); // test non atteind
+	return nbSejours - 2; // scotch de tes morts
 }
 
 void afficherListeSejours(int tailleMax, sejour sejours[]) {
-	int i;
+	int i, j;
 	system("cls"); // on vide la console
 	printf("==========================================================\n");
 	printf("%s", Accent("================= Liste des séjours ======================\n"));
 	printf("==========================================================\n");
 	i = lectureSejours(tailleMax, sejours);
 	printf("%d\n", i);
+	for(j = 1 ; j <= i ; j++) {
+		printf("%d %d %d %s %6.2f\n", sejours[j].id, sejours[j].formule, sejours[j].nbPersonnes, sejours[j].date, sejours[j].prix); 
+	}
 	system("pause");
 }
 
--- structures/sejour.h
+++ structures/sejour.h
@@ -5,7 +5,7 @@
 typedef struct sejour{
 	short id;
 	short formule;						//1 pour tente , 2 pour caravane, 3 pour bungalow
-	short nbPersonne;					//nombre de personnes pour qui
+	short nbPersonnes;					//nombre de personnes pour qui
 	char date[11];						//sous le format dd/mm/yyyy
 	float prix;							//prix du séjour
 	short electricite;					//1 si il faut de l'électricité, 0 si non
