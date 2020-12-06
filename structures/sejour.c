9b73ac0728deda410d26485d5a1cdd09157ec59e
scryper <florian.mazzeo@gmail.com>
2020-12-06 20:28:49 +0100

    jpp

--- structures/sejour.c
+++ structures/sejour.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "sejour.h"
 #include "emplacement.h"
 
-void viderSejours(sejour sejours[], int tailleMax){
-	int i;
-	for (i = 1 ; i < tailleMax ; i++){
-		sejours[i].id = 0;
-	}
+sejour *premierSejour, *sejourCourant, *sejourSuivant, *sejourIntercale;
+
+void viderSejours(){
+	printf("sdfg");
 }
 
-int lectureSejours(int tailleMax, sejour sejours[]) {
+int lectureSejours(sejour *courant) {
 	printf("test\n");
-	int nbSejours = 1, jour, mois, annee, tmp, tmp2;
+	int nbSejours = 1, jour, mois, annee, idSuivant, idEmplacement;
 	char tmpJour[3], tmpMois[3], tmpAnnee[5], tmpDate[11];
+	
 	FILE *fSejour;
 	fSejour = fopen("data/sejour.dat", "r");
+	
+	sejourCourant = malloc(sizeof(sejour));
+	premierSejour = sejourCourant;
+
 	while(!feof(fSejour)) {
-		fscanf(fSejour, "%d %d %d %2d %2d %4d %f %d %d", &sejours[nbSejours].id, &sejours[nbSejours].formule, &sejours[nbSejours].nbPersonnes, &jour, &mois, &annee, &sejours[nbSejours].prix, &tmp, &tmp2); 
+		fscanf(fSejour, "%d %d %d %2d %2d %4d %f %d %d %d", &courant->id, &courant->formule, &courant->nbPersonnes, &jour, &mois, &annee, &courant->idClient, &courant->prix, &idSuivant, &idEmplacement); 
 		// conversion entier -> char pour créer la date
-		itoa(jour, tmpJour, 10);
+		itoa(jour, tmpJour, 10); // on copie tmpJour dans jour, en suivant la base 10
 		itoa(mois, tmpMois, 10);
 		itoa(annee, tmpAnnee, 10);
 		// création de la date
@@ -28,27 +33,30 @@ int lectureSejours(int tailleMax, sejour sejours[]) {
 		strcat(tmpDate, tmpMois);
 		strcat(tmpDate, "/");
 		strcat(tmpDate, tmpAnnee);
-		strcpy(sejours[nbSejours].date, tmpDate);
-		printf("%d %d %d %s %6.2f\n", sejours[nbSejours].id, sejours[nbSejours].formule, sejours[nbSejours].nbPersonnes, sejours[nbSejours].date, sejours[nbSejours].prix);
+		strcpy(courant->date, tmpDate);
+		printf("%d %d %d %s %6.2f\n", courant->id, courant->formule, courant->nbPersonnes, courant->date, courant->prix);
 		nbSejours++;
+		sejourSuivant = malloc(sizeof(sejour));
+		courant->nxtSej = sejourSuivant;
+   	  	courant = sejourSuivant;
 		printf("test 2\n");
 		// problème de boucle 
 	}
 	fclose(fSejour);
-	printf("test  3\n"); // test non atteind
-	return nbSejours - 2; // scotch de tes morts
+	printf("test  3\n");
+	return nbSejours - 1; // retrun incorrect, corriger. enfin quand je pourrais compiler
 }
 
-void afficherListeSejours(int tailleMax, sejour sejours[]) {
-	int i, j;
+void afficherListeSejours() {
+	int nb, i;
 	system("cls"); // on vide la console
 	printf("==========================================================\n");
 	printf("%s", Accent("================= Liste des séjours ======================\n"));
 	printf("==========================================================\n");
-	i = lectureSejours(tailleMax, sejours);
-	printf("%d\n", i);
-	for(j = 1 ; j <= i ; j++) {
-		printf("%d %d %d %s %6.2f\n", sejours[j].id, sejours[j].formule, sejours[j].nbPersonnes, sejours[j].date, sejours[j].prix); 
+	nb = lectureSejours(sejourCourant);
+	//printf("%d %d %d %s %6.2f\n", sejours[j].id, sejours[j].formule, sejours[j].nbPersonnes, sejours[j].date, sejours[j].prix); 
+	for(i = 1 ; i <= nb ; i++) {
+		
 	}
 	system("pause");
 }
@@ -57,11 +65,11 @@ void afficherEmplacementsLibres() {
 	printf("liste des emplacements libres");
 }
 
-void switchMenuSejour(int choix, int tailleMax, sejour sejours[]) {
+void switchMenuSejour(int choix) {
 	switch(choix) {
 		case 1:
 			// liste des séjours
-			afficherListeSejours(tailleMax, sejours);
+			afficherListeSejours();
 			break;
 		case 2: 
 			// emplacements libre
--- structures/sejour.h
+++ structures/sejour.h
@@ -4,6 +4,7 @@
 //définition d'un séjour
 typedef struct sejour{
 	short id;
+	short idClient;
 	short formule;						//1 pour tente , 2 pour caravane, 3 pour bungalow
 	short nbPersonnes;					//nombre de personnes pour qui
 	char date[11];						//sous le format dd/mm/yyyy
@@ -15,8 +16,8 @@ typedef struct sejour{
 
 void afficherListeSejours();
 void afficherEmplacementsLibres();
-int lectureSejours(int, sejour[]);
-void viderSejours(sejour[], int);
-void switchMenuSejour(int, int, sejour[]);
+int lectureSejours(sejour*);
+void viderSejours();
+void switchMenuSejour(int);
 
 #endif
